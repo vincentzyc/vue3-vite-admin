9228d717f876b51559d9509c01df0c31b2dd9ee7
vincentzyc <vincentzyc@qq.com>
2023-03-01 10:22:23 +0800

    🎉 init: init

---
+++ src/utils/loading.ts
@@ -0,0 +1,18 @@
+import { ElLoading } from 'element-plus'
+
+export function openLoading(text?: string): void {
+  window.fullLoading = ElLoading.service({
+    lock: true,
+    text: text,
+    background: 'rgba(0, 0, 0, 0.66)'
+  });
+}
+
+export function closeLoading(): void {
+  if (window.fullLoading) window.fullLoading.close()
+}
+
+export default {
+  open: openLoading,
+  close: closeLoading
+}
---
+++ src/utils/storage.ts
@@ -0,0 +1,49 @@
+import { decrypt, encrypt } from "./crypto";
+
+export function setSessionStorage(key: string, value: any, encode = false): void {
+  let str = window.JSON.stringify(value);
+  if (encode) {
+    str = encrypt(str);
+  }
+  window.sessionStorage.setItem(key, str);
+}
+
+export function getSessionStorage(key: string, decode = false): any {
+  let str: string | null;
+  str = window.sessionStorage.getItem(key);
+  if (!str) return "";
+  if (decode) {
+    str = decrypt(str);
+  }
+  if (!str) return "";
+  try {
+    return window.JSON.parse(str);
+  } catch (error) {
+    window.sessionStorage.removeItem(key);
+    window.location.reload();
+  }
+}
+
+export function setLocalStorage(key: string, value: any, encode = false): void {
+  let str = window.JSON.stringify(value);
+  if (encode) {
+    str = encrypt(str);
+  }
+  window.localStorage.setItem(key, str);
+}
+
+export function getLocalStorage(key: string, decode = false): any {
+  let str: string | null;
+  str = window.localStorage.getItem(key);
+  if (!str) return "";
+  if (decode) {
+    str = decrypt(str);
+  }
+  if (!str) return "";
+  try {
+    return window.JSON.parse(str);
+  } catch (error) {
+    window.localStorage.removeItem(key);
+    window.location.reload();
+  }
+}
