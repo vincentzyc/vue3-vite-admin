3623612f9bb2598d6e9e2aefec533993973ed690
vincentzyc <vincentzyc@qq.com>
2023-03-03 10:09:46 +0800

    🎉 init: init

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare module 'element-plus/dist/locale/zh-cn.mjs'
+
+// declare const murmur: string
+interface Window {
+  _paq: any; // matomo
+  fullLoading: any,
+  dom_container: HTMLElement | null,
+  dom_handle_sidebar: HTMLElement | null
+}
+
