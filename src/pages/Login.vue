8e548ef5a512c536c02e69a5bc9853f997b1701e
vincentzyc <vincentzyc@qq.com>
2023-03-04 22:48:00 +0800

    🎉 init: init

---
+++ src/pages/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login-wrap"></div>
+</template>
+
+<script setup lang="ts">
+import { CommonApi, getLoginUrl } from "@/api";
+import { USERINFO } from "@/assets/js/storage-keys";
+import { getUrlParam } from "@/utils";
+import { closeLoading, openLoading } from "@/utils/loading";
+import { getSessionStorage, setLocalStorage } from "@/utils/storage";
+
+const getUserInfo = async (token: string) => {
+  openLoading();
+  let res = await CommonApi.checkToken('?token=' + token)
+  let crmMenus = await CommonApi.getSideBar({ userId: res.id });
+  let userInfo = {
+    crmMenus: crmMenus,
+    ...res.data
+  };
+  setLocalStorage(USERINFO, userInfo, true);
+  closeLoading();
+  let backUrl = getSessionStorage("backUrl");
+  if (backUrl) {
+    sessionStorage.removeItem("backUrl");
+    window.location.href = backUrl;
+    return;
+  }
+  window.location.href = window.location.origin + window.location.pathname
+}
+
+const token = getUrlParam('token');
+if (token) {
+  getUserInfo(token);
+} else {
+  window.location.href = await getLoginUrl();
+}
+</script>
