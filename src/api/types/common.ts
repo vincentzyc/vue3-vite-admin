53d6667190a871ed58ea19190f5d1dc9dbadde98
vincentzyc <vincentzyc@qq.com>
2023-03-05 10:56:55 +0800

    🎉 init: init

---
+++ src/api/types/common.ts
@@ -0,0 +1,7 @@
+export interface CheckToken {
+  id: number;
+  data: any
+}
+export type Logout = any
+export type GetSideBar = any
+
---
+++ src/api/types/tagManage.ts
@@ -0,0 +1,17 @@
+interface ListLabelChildren {
+  id: number;
+  name: string;
+  type: string;
+  children: ListLabelChildren
+}
+
+export interface ListLabel {
+  id: number;
+  name: string;
+  type: string;
+  children: ListLabelChildren[];
+}
+
+export type AddLabel = boolean
+export type GetSubProductList = string[]
+export type GetSubtypeList = string[]
