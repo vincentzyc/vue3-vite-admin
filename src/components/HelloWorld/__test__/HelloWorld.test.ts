52072cc07a70520ced0d3b52b06b177aceb64486
vincentzyc <vincentzyc@qq.com>
2023-03-06 09:31:15 +0800

    🎉 init: init

---
+++ src/components/HelloWorld/__test__/HelloWorld.test.ts
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld/HelloWorld.vue'
+import { ElButton } from 'element-plus' 
+
+test('hello world mount', async () => {
+  expect(HelloWorld).toBeTruthy()
+
+  const wrapper = mount(HelloWorld, {
+    props: {
+      count: 4,
+    },
+    global: {
+      components: {
+        ElButton
+      }
+    }
+  })
+
+  expect(wrapper.text()).toContain('4 x 2 = 8')
+  expect(wrapper.html()).toMatchSnapshot()
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 3 = 12')
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 4 = 16')
+})
