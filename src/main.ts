b4e9a7e3452662ba2bcfde254046d8b50bb64f03
vincentzyc <vincentzyc@qq.com>
2023-03-09 10:18:17 +0800

    🎉 init: init

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import Axios from './plugins/axios'
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import { createPinia } from 'pinia'
-import '@/assets/css/main.less'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import Axios from './plugins/axios';
+import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+import { createPinia } from 'pinia';
+import '@/assets/css/main.less';
 
 if (process.env.NODE_ENV !== 'production') {
-  import('element-plus/dist/index.css')
+  import('element-plus/dist/index.css');
 }
 
 const app = createApp(App);
 
-app.use(router)
-app.use(createPinia())
+app.use(router);
+app.use(createPinia());
 app.use(ElementPlus, {
   locale: zhCn,
-})
+});
 
-app.config.globalProperties.$axios = Axios
+app.config.globalProperties.$axios = Axios;
 
-app.mount('#app')
+app.mount('#app');
