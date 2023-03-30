8b7840ad247c93abf7f7875c25234a06313cb706
vincentzyc <vincentzyc@qq.com>
2023-03-30 19:12:06 +0800

    🎉init: init

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex flex-center" style="height: 100%">
     <img src="@/assets/img/404.jpg" alt="404" />
+    <h1>{{ text }}</h1>
   </div>
 </template>
+<script setup lang="ts">
+const text = ref('404');
+</script>
